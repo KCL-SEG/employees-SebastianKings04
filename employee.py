d96c1c4dc343e43340fcbe448bbec47d6920345b
Sebastian Habram <sebastian.habram@kcl.ac.uk>
2023-09-26 20:24:48 +0100

    Change Employee to have all functionality to complete task

--- employee.py
+++ employee.py
@@ -2,30 +2,70 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+
+    def __init__(self, name, contractRate = "", contractNumber = ""):
         self.name = name
+        self.contractRate = contractRate
+        self.contractNumber = contractNumber
 
     def get_pay(self):
         pass
 
     def __str__(self):
         return self.name
+    
+class MonthlyEmployee(Employee):
+
+    def __init__(self, name, monthlyPay, contractRate = "", contractNumber = ""):
+        super().__init__(name, contractRate, contractNumber)
+        self.monthlyPay = monthlyPay
+    
+    def get_pay(self):
+        return self.monthlyPay
+    
+    def __str__(self):
+        sentence = f"{self.name} works on a monthly salary of {self.monthlyPay} "
+        if self.contractNumber:
+            sentence += f"and recieves a commision for {self.contractNumber} contract(s) at {self.contractRate}/contract"
+        elif self.contractRate:
+            sentence += f"and recieves a bonus commission of {self.contractRate}"
+        sentence += f". Their total pay is {self.get_pay()}"
+        return sentence
+
+class ContractEmployee(Employee):
+
+    def __init__(self, name, hourlyPay, hours, contractRate = "", contractNumber = ""):
+        super().__init__(name, contractRate, contractNumber)
+        self.hourlyPay = hourlyPay
+        self.hours = hours
+    
+    def get_pay(self):
+        return self.hourlyPay * self.hours
+
+    def __str__(self):
+        sentence = f"{self.name} works on a contract of {self.hours} hours at {self.hourlyPay}/hour "
+        if self.contractNumber:
+            sentence += f"and recieves a commision for {self.contractNumber} contract(s) at {self.contractRate}/contract"
+        elif self.contractRate:
+            sentence += f"and recieves a bonus commission of {self.contractRate}"
+        sentence += f". Their total pay is {self.get_pay()}"
+        return sentence
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+billie = MonthlyEmployee('Billie', 4000)
 
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+charlie = ContractEmployee('Charlie', 25, 100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+renee = MonthlyEmployee('Renee', 3000, 200, 4)
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+jan = ContractEmployee('Jan', 25, 150, 220, 3)
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+robbie = MonthlyEmployee('Robbie', 2000, 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+ariel = ContractEmployee('Ariel', 30, 120, 600)
