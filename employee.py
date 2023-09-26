26fc4af43e70d7c83903d27407c6fc1a97505c91
Sebastian Habram <sebastian.habram@kcl.ac.uk>
2023-09-26 21:33:49 +0100

    Fix spelling mistakes of commission and receives

--- employee.py
+++ employee.py
@@ -31,9 +31,9 @@ class MonthlyEmployee(Employee):
     def __str__(self):
         sentence = f"{self.name} works on a monthly salary of {self.monthlyPay}"
         if self.contractNumber:
-            sentence += f" and recieves a commision for {self.contractNumber} contract(s) at {self.contractRate}/contract"
+            sentence += f" and receives a commission for {self.contractNumber} contract(s) at {self.contractRate}/contract"
         elif self.contractRate:
-            sentence += f" and recieves a bonus commission of {self.contractRate}"
+            sentence += f" and receives a bonus commission of {self.contractRate}"
         sentence += f". Their total pay is {self.get_pay()}."
         return sentence
 
@@ -55,9 +55,9 @@ class ContractEmployee(Employee):
     def __str__(self):
         sentence = f"{self.name} works on a contract of {self.hours} hours at {self.hourlyPay}/hour"
         if self.contractNumber:
-            sentence += f" and recieves a commision for {self.contractNumber} contract(s) at {self.contractRate}/contract"
+            sentence += f" and receives a commission for {self.contractNumber} contract(s) at {self.contractRate}/contract"
         elif self.contractRate:
-            sentence += f" and recieves a bonus commission of {self.contractRate}"
+            sentence += f" and receives a bonus commission of {self.contractRate}"
         sentence += f". Their total pay is {self.get_pay()}."
         return sentence
 
@@ -70,15 +70,14 @@ charlie = ContractEmployee('Charlie', 25, 100)
 
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = MonthlyEmployee('Renee', 3000, 200, 4)
-print(renee.get_pay())
 
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = ContractEmployee('Jan', 25, 150, 220, 3)
-print(jan.get_pay())
 
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = MonthlyEmployee('Robbie', 2000, 1500)
-print(robbie.get_pay())
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = ContractEmployee('Ariel', 30, 120, 600)
+
+
